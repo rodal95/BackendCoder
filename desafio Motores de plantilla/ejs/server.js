f19abd2fa7d933c62f4802c28efbfeb9ea5130c1
Rodal 95 <alday559@gmail.com>
2022-10-15 17:57:59 -0300

    tratando de hacer desafio

---
+++ desafio Motores de plantilla/ejs/server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+app.listen(8080, ()=>console.log("andando wacho"))
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.set("views", "./views")
+app.set("view engine","ejs")
+
+
+const productos = []
+app.get("/", (req,res)=>{
+    res.render("home")
+})
+app.get("/formulario",(req,res)=>{
+    res.render("form")
+})
+app.get("/tabla",(req,res)=>{
+    res.render("table")
+})
+
+app.post("/formulario",(req,res)=>{
+    const newProduct = req.body
+    console.log(newProduct)
+    /* productos.push(newProduct) */
+    res.redirect("/")
+})
+
+
+
+
