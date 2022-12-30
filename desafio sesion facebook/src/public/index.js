ea463f79c30131242440fe42e9b9d9c0c695beb2
Rodal 95 <alday559@gmail.com>
2022-12-30 17:12:30 -0300

    terminando desafio

---
+++ desafio sesion facebook/src/public/index.js
@@ -0,0 +1,93 @@
+
+//punto de comunicacion cliente
+const socketClient = io()
+//captura el valor del usuario
+let user
+
+function salir(){
+    window.location.replace("/salir")
+
+  }
+socketClient.on("usuarioLog",(data)=>{
+   user = data
+   console.log(user.nombre)
+   
+const containerUsuario = document.getElementById("UsuarioLog")
+
+containerUsuario.innerHTML=`<h1>Bienvenido ${user.nombre}</h1><a href='/salir'><button>Salir</button></a>`
+
+  })
+
+//guardar un productos
+const productForm = document.getElementById("productForm")
+productForm.addEventListener("submit",(evt)=>{
+    //prevenir q se refresque
+    evt.preventDefault()
+    const product = {
+        title:document.getElementById("title").value,
+        price:document.getElementById("price").value,
+        image:document.getElementById("image").value
+    }
+    title.value=""
+    price.value=""
+    image.value=""
+    //enviamos el nuevo producto al servidor
+    socketClient.emit("newProduct",product)
+})
+
+//productos en tiempo real
+const createTabla = async (data)=>{
+    const response = await fetch("./templates/tabla.hbs")
+    const result = await response.text()
+    const template = Handlebars.compile(result)
+    const html = await template({products:data})
+    return html
+}
+
+const productsContainer = document.getElementById("productsContainer")
+socketClient.on("products",async (data)=>{
+   const htmlProducts =  await createTabla(data)
+   productsContainer.innerHTML= htmlProducts
+})
+
+const authorSchema = new normalizr.schema.Entity("authors",{}, {idAttribute:"email"})
+const mensajeSchema = new normalizr.schema.Entity("mensajes",{author:authorSchema})
+const chatSchema = new normalizr.schema.Entity("chat",{
+    mensajes:[mensajeSchema]
+},{idAttribute:"id"})
+
+
+//logica del chat
+//enviar mensaje
+const campo = document.getElementById("messageField")
+
+campo.addEventListener("keydown",(evt)=>{
+    /* console.log(evt.key) */
+    if(evt.key === "Enter"){
+        socketClient.emit("message", {
+            author:user,
+            text:campo.value,
+            timestamp: new Date().toLocaleDateString()
+    })
+    
+    campo.value=""
+    }
+})
+//mostrar todos los mensajes cuando usuario carga pag
+const messageContainer = document.getElementById("messageContainer")
+socketClient.on("historico", (data)=>{
+    let elementos =""
+    const normalData = normalizr.denormalize(data.result,chatSchema,data.entities)
+    normalData.mensajes.forEach(item =>{
+        elementos = elementos + `<p><strong>${item.author.nombre}</strong>: ${item.text}</p>`
+        
+    })
+    messageContainer.innerHTML = elementos
+})
+
+/* socketClient.on("newUser",()=>{
+    Swal.fire({
+        text:"nuevo usuario en linea",
+        toas:true
+    })
+}) */
---
+++ desafio sesion facebook/src/server.js
@@ -0,0 +1,218 @@
+const express = require("express");
+const handlebars = require("express-handlebars")
+const options = require("./config/dbConfig")
+const {Server} = require("socket.io");
+const Contenedor = require("./managers/contenedor")
+const ContenedorMensajes = require("./managers/contenedorMensajes")
+const {normalize,schema} = require("normalizr")
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const mongoose = require("mongoose")
+const MongoStore = require("connect-mongo");
+const passport = require("passport")
+const {Strategy}  = require("passport-local")
+const {userModel} = require("./models/user")
+/* const ContenedorSql = require("./managers/contenedorSql") */
+const mongoUrl = "mongodb+srv://RodrigoHalo:CoderProyecto@coderbackend.fgpchrr.mongodb.net/authDB?retryWrites=true&w=majority"
+
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+},(error)=>{
+    if(error) return console.log("hubo un error",error)
+    console.log("conexion exitosa")
+})
+
+//servicios
+const productsServices = new Contenedor("productos.txt")
+/* const productsServices = new ContenedorSql(options.mariaDB,"products") */
+const mensajesApi = new ContenedorMensajes("mensajes.txt")
+/* const mensajesApi = new ContenedorSql(options.sqliteDB,"chat") */
+const app = express();
+app.engine(".hbs",handlebars.engine({extname: '.hbs'}));
+//ruta de las vistas
+app.set("views", __dirname+"/views");
+//vinculacion del motor a express
+app.set("view engine", ".hbs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 8080;
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl:"mongodb+srv://RodrigoHalo:CoderProyecto@coderbackend.fgpchrr.mongodb.net/sessionDB?retryWrites=true&w=majority"
+    }),
+    secret:"claveSecreta", //clave de encriptacion de la sesion
+    //config para guardar en la memoria del servidor
+    resave:false,
+    saveUninitialized:false,
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+//configuracion de passport
+passport.serializeUser((user,done)=>{
+    done(null,user.id)
+})
+passport.deserializeUser((id,done)=>{
+    userModel.findById(id,(err,userFound)=>{
+        if(err) return done(err)
+        return done(null,userFound)
+    })
+})
+
+//funcion encriptar contraseña
+const createHash = (password)=>{
+    const hash = bcrypt.hashSync(password,bcrypt.genSaltSync(10))
+    return hash
+}
+
+
+passport.use("signupStrategy",new Strategy(
+    {
+        passReqToCallback:true,
+        usernameField:"email"
+    },
+    (req,username,password,done)=>{
+        //logica para recibir usuario
+        userModel.findOne({username:username},(err,userFound)=>{
+            if(err) return done(err,null,{message:"hubo un error"})
+            if(userFound) return done(null,null,{message:"el user ya existe"})
+            //guardamos el usuario en db
+            const newUser={
+                name:req.body.name,
+                username:username,
+                password:createHash(password)
+            }
+            userModel.create(newUser,(error,userCreated)=>{
+                if(error) return done(error,null,{message:"error al registrar user"})
+                return done(null,userCreated)
+            })
+        })
+    }
+))
+let userLog
+//servidor de express
+const server = app.listen(PORT, ()=>console.log(`listening on port ${PORT}`));
+
+//servidor de websocket y lo conectamos con el servidor de express
+const io = new Server(server);
+//trabajar con archivos estaticos de public
+app.use(express.static(__dirname+"/public"));
+app.use(cookieParser());
+
+
+app.get("/",(req,res)=>{
+    res.render("login")
+})
+app.get("/home",(req,res)=>{
+    if(req.session.user){
+        res.render("index")
+    }else{
+        res.redirect("/login")
+    }
+})
+app.get("/login",async (req,res)=>{
+    res.render("login")
+} )
+app.post("/login",(req,res)=>{
+    if(req.session.user || req.isAuthenticated()){}
+    const user = req.body
+    userLog=user
+    userModel.findOne({username:user.email},(err,userFound)=>{
+        if(err) res.send(err)
+        if(userFound){
+            if(bcrypt.compareSync(user.password,userFound.password)){
+                req.session.user = user
+                res.redirect("/home")
+            }else{
+                res.render("login",{error:"contraseña incorrecta gil"})
+            }
+        }else{
+            res.render("login",{error:"usuario no registrado en base"})
+        }
+    })
+});
+app.get("/registro",(req,res)=>{
+    if(req.session.user || req.isAuthenticated()){
+        res.redirect("/perfil")
+    }else{
+        res.render("signup")
+    }
+});
+
+app.post("/signup",passport.authenticate("signupStrategy",{
+    failureRedirect:"/registro",
+    failureMessage:true
+}),(req,res)=>{
+    res.render("signup",{message:"usuario registrado con exito, vaya a iniciar sesion"})
+});
+
+app.get("/logout",(req,res)=>{
+    req.logOut(err=>{
+        if(err) return res.send("error en cerrar sesion")
+        req.session.destroy();
+        res.redirect("/")
+    }) 
+    
+});
+
+let historicosMensajes = []
+
+const authorSchema = new schema.Entity("authors",{}, {idAttribute:"email"})
+const mensajeSchema = new schema.Entity("mensajes",{author:authorSchema})
+const chatSchema = new schema.Entity("chat",{
+    mensajes:[mensajeSchema]
+},{idAttribute:"id"})
+
+
+const normalizarData = (data)=>{
+    const normalizeData = normalize({id:"chathistory", mensajes:data},chatSchema)
+    return normalizeData
+}
+const normalizarMensajes = async()=>{
+    const results = await mensajesApi.getAll()
+    const messagesNormalize = normalizarData(results)
+    return messagesNormalize
+
+}
+normalizarMensajes()
+//socket
+io.on("connection",async (socket)=>{
+
+    socket.emit("usuarioLog", userLog)
+    //enviar todos los prod cuando se conecte
+    socket.emit("products", await productsServices.getAll())
+
+    //recibimos el producto del cliente y guardamos
+    socket.on("newProduct",async (data)=>{
+        await productsServices.save(data)
+        //enviamos la lista actualizada a todos los sockets
+        io.sockets.emit("products", await productsServices.getAll())
+    })
+
+    socket.broadcast.emit("newUser")
+    socket.emit("historico",await normalizarMensajes())
+    socket.on("message",async data => {
+        if(historicosMensajes.length==0){
+            nuevoMensaje = {
+                id:1,
+                ...data
+            }
+            
+            historicosMensajes.push(nuevoMensaje)
+            await mensajesApi.save(historicosMensajes)
+        }
+        else{
+            nuevoMensaje ={
+                id:historicosMensajes.length+1,
+                ...data
+            }
+            historicosMensajes.push(nuevoMensaje)
+            await mensajesApi.save(historicosMensajes)
+        }
+        
+       await io.sockets.emit("historico",await normalizarMensajes())
+       
+    })
+})
